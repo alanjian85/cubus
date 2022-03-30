44c3c6a1abb402f3a75cf9e9e722fc4d745eb9d4
Alan Jian <alanjian85@outlook.com>
2022-03-30 22:09:30 +0800

    Implement all planes

--- cephalon/src/utils/frustum.cc
+++ cephalon/src/utils/frustum.cc
@@ -3,14 +3,18 @@ using namespace cephalon;
 
 Frustum::Frustum(PerspectiveCamera cam) {
     auto tan = glm::tan(glm::radians(cam.fov));
-    auto near_height = cam.near * tan * 0.5f;
-    auto near_width = cam.aspect * near_height;
-    auto far_height = cam.far * tan * 0.5f;
-    auto far_width = cam.aspect * far_height;
+    auto height = cam.far * tan * 0.5f;
+    auto width = cam.aspect * height;
 
+    auto length_pos = glm::length(cam.pos);
+    auto far = cam.dir * cam.far;
     auto near_center = cam.pos + cam.dir * cam.near;
-    auto far_center = cam.pos + cam.dir * cam.far;
+    auto far_center = cam.pos + far;
 
-    planes_[0] = Plane(cam.dir, glm::length(near_center)); // near
-    planes_[1] = Plane(cam.dir, glm::length(far_center)); // far
+    planes_[0] = Plane(glm::normalize(glm::cross(cam.up, far + cam.right * width)), length_pos); // right
+    planes_[1] = Plane(glm::normalize(glm::cross(far - cam.right * width, cam.up)), length_pos); // left
+    planes_[2] = Plane(glm::normalize(glm::cross(cam.right, far - cam.up * height)), length_pos); // top
+    planes_[3] = Plane(glm::normalize(glm::cross(far + cam.up * height, cam.right)), length_pos); // bottom
+    planes_[4] = Plane(-cam.dir, glm::length(far_center)); // far
+    planes_[5] = Plane(cam.dir, glm::length(near_center)); // near
 }
