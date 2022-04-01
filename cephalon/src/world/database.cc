43d444520906eae54adad8d2890e52777f9671d0
Alan Jian <alanjian85@outlook.com>
2022-04-01 20:09:00 +0800

    Optimize database

--- cephalon/src/world/database.cc
+++ cephalon/src/world/database.cc
@@ -18,14 +18,16 @@ Database::Database(const char* path) {
     }
     const char* create_query =
         "CREATE TABLE IF NOT EXISTS blocks ("
-        "x    INT NOT NULL,"
-        "y    INT NOT NULL,"
-        "z    INT NOT NULL,"
+        "rx INT NOT NULL,"
+        "ry INT NOT NULL,"
+        "ox INT NOT NULL,"
+        "oy INT NOT NULL,"
+        "oz INT NOT NULL,"
         "name TEXT NOT NULL"
         ");"
-        "CREATE UNIQUE INDEX IF NOT EXISTS blocks_xyz_idx ON blocks (x, y, z)";
-    const char* insert_query = "REPLACE INTO blocks (x, y, z, name) VALUES (?, ?, ?, ?)";
-    const char* load_query = "SELECT x, y, z, name FROM blocks;";
+        "CREATE UNIQUE INDEX IF NOT EXISTS blocks_position ON blocks (rx, ry, ox, oy, oz)";
+    const char* insert_query = "REPLACE INTO blocks (rx, ry, ox, oy, oz, name) VALUES (?, ?, ?, ?, ?, ?)";
+    const char* load_query = "SELECT ox, oy, oz, name FROM blocks WHERE rx = ? AND ry = ?;";
     rc = sqlite3_exec(db_, create_query, nullptr, nullptr, nullptr);
     if (rc) spdlog::error("Failed to execute table create query");
     else spdlog::info("Table created successfully");
@@ -51,32 +53,37 @@ Database::~Database() {
 }
 
 void Database::loadChunk(Chunk& chunk) const {
+    auto region = chunk.getRegion();
     std::lock_guard lock(load_mutex_);
     sqlite3_reset(load_stmt_);
+    sqlite3_bind_int(load_stmt_, 1, region.x);
+    sqlite3_bind_int(load_stmt_, 2, region.y);
     while (sqlite3_step(load_stmt_) == SQLITE_ROW) {
-        auto x = sqlite3_column_int(load_stmt_, 0);
-        auto y = sqlite3_column_int(load_stmt_, 1);
-        auto z = sqlite3_column_int(load_stmt_, 2);
-        glm::ivec3 pos(x, y, z);
+        auto ox = sqlite3_column_int(load_stmt_, 0);
+        auto oy = sqlite3_column_int(load_stmt_, 1);
+        auto oz = sqlite3_column_int(load_stmt_, 2);
+        glm::ivec3 offset(ox, oy, oz);
         auto name = sqlite3_column_text(load_stmt_, 3);
         auto block = Block::getBlock(reinterpret_cast<const char*>(name));
         if (!block) {
             spdlog::warn("Unknown block name {}", name);
             continue;
         }
-        if (World::getRegion(pos) == chunk.getRegion()) {
-            chunk.setBlock(World::getOffset(pos), *block);
-        }
+        chunk.setBlock(offset, *block);
     }
 }
 
 void Database::insertBlock(glm::ivec3 pos, const char* name) {
+    auto region = World::getRegion(pos);
+    auto offset = World::getOffset(pos);
     std::lock_guard lock(insert_mutex_);
     sqlite3_reset(insert_stmt_);
-    sqlite3_bind_int(insert_stmt_, 1, pos.x);
-    sqlite3_bind_int(insert_stmt_, 2, pos.y);
-    sqlite3_bind_int(insert_stmt_, 3, pos.z);
-    sqlite3_bind_text(insert_stmt_, 4, name, -1, nullptr);
+    sqlite3_bind_int(insert_stmt_, 1, region.x);
+    sqlite3_bind_int(insert_stmt_, 2, region.y);
+    sqlite3_bind_int(insert_stmt_, 3, offset.x);
+    sqlite3_bind_int(insert_stmt_, 4, offset.y);
+    sqlite3_bind_int(insert_stmt_, 5, offset.z);
+    sqlite3_bind_text(insert_stmt_, 6, name, -1, nullptr);
     int rc = sqlite3_step(insert_stmt_);
     if (rc != SQLITE_DONE) spdlog::critical("Failed to execute block insert statement");
     else spdlog::debug("Block inserted successfully");
